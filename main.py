0b85727e89c4ab7fd01718ff705d8c698c84d2ec
Said-Alisic <said0390@stud.kea.dk>
2019-04-17 23:04:42 +0200

    Added HTML file

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # Import Flask module from flask package - creating Flask web server
 from flask import Flask
 
-# creating instance of Flask class and naming it app - "__name__" refers to the current file
+# Creating instance of Flask class and naming it app - "__name__" refers to the current file
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
+@app.route("/")     # Represents default page
+def home():         # Function activated after going to default page
     return "Hello, World!"
 
-    
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+
+# Python assigns the name "__main__" to script when executed
+# if-statement prevents other scripts (python files) from running
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)     # Runs app and debug=True allows Python errors to appear on the web page
 
